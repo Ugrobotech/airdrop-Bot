c6b9a8501853dcd38e751b807d78c50837591745
ceasar28 <emmanuelugochukwuekete@gmail.com>
2024-02-21 02:56:04 +0100

    to send images and writeup saperately

--- src/bot/bot.service.ts
+++ src/bot/bot.service.ts
@@ -160,7 +160,7 @@ export class BotService {
         reply_markup: markup,
       });
     } catch (error) {
-      console.error('caption error');
+      console.log('caption error');
       // to send the image and writeups saperately
       const sendSaperately = async () => {
         await this.bot.sendPhoto(userId, imageUrl, {
